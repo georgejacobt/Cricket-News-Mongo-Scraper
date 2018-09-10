0479301676ce23cfe42de6f1bff29d8181782597
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-09-10 17:25:50 -0500

    changes

--- public/Javascript/app.js
+++ public/Javascript/app.js
@@ -107,18 +107,6 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-$(document).on("click", "#scrape-clear", function() {
-
-  $("#articles").empty();
-
-});
-
-
 $(document).on("click", "#scrape-now", function(){
 
   $.ajax({
@@ -140,40 +128,6 @@ $(document).on("click", "#refresh-page", function() {
 
 
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
 
 
 // When you click the savenote button
@@ -194,12 +148,38 @@ $(document).on("click", "#save-article", function() {
     .then(function(data) {
       // Log the response
       console.log(data);
+      window.location.reload();
     
     });
 
 });
 
+
+$(document).on("click", "#delete-article", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("article-id");
+  
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/unsave/" + thisId,
+    data: {
+      saved: false
+    }
+  })
+   
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      window.location.reload();
+    
+    });
+
 });
 
 
 
+
+
+});
